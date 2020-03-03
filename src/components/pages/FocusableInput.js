c92ed616450b2a379c444b86014f04042644b9b3
Julián Absatz <julianabsatz@gmail.com>
2020-03-03 01:55:17 +0100

    more styles and annotations

--- src/components/pages/FocusableInput.js
+++ src/components/pages/FocusableInput.js
@@ -1,11 +1,14 @@
 import React, { useRef } from 'react';
+import FancyButton from '../small/FancyButton';
+import './FocusableInput.css';
 
 const FocusableInput = () => {
   const inputRef = useRef();
   return (
-    <div>
-      <input ref={inputRef} />
-      <button onClick={() => inputRef.current.focus()}>Focus that input!</button>
+    <div className="focusable-input-wrapper">
+      {/* Qué pasa si cambiamos este input por nuestro FancyInput? */}
+      <input className="focusable-input" ref={inputRef} />
+      <FancyButton onClick={() => inputRef.current.focus()}>Focus that input!</FancyButton>
     </div>
   );
 };
